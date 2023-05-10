f046ee11b2e840907bbb8b71d3d184711101b736
alexiszook <zookam21@gcc.edu>
2023-05-09 21:33:06 -0400

    removed exception from main

--- src/Play.java
+++ src/Play.java
@@ -1,5 +1,5 @@
 public class Play {
-    public static void main(String args[]) throws Exception {
+    public static void main(String args[]) {
         Game game = new Game();
         game.gameLoop();
 
